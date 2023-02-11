936cdc9b0b6ff1e1c3585cd988e94a82a73c1499
Im Seung-yeon <122198365+Imseungyeon@users.noreply.github.com>
2023-02-11 16:34:09 +0900

    Create 영어가_싫어요.java

---
+++ Programmers/level0/영어가_싫어요.java
@@ -0,0 +1,10 @@
+class Solution {
+    public long solution(String numbers) {
+        String[] strArr = {"zero", "one", "two", "three", "four", 
+                            "five", "six", "seven", "eight", "nine"};
+        for (int i = 0; i < strArr.length; i++){
+            numbers = numbers.replace(strArr[i], String.valueOf(i));
+        }
+        return Long.parseLong(numbers);
+    }
+}
