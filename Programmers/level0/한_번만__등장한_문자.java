9a9f7d58bfe395375393335968e8ba22c4b1aeac
Im Seung-yeon <122198365+Imseungyeon@users.noreply.github.com>
2023-02-15 17:48:44 +0900

    Create 한_번만__등장한_문자.java

---
+++ Programmers/level0/한_번만__등장한_문자.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        int[] count = new int[26];
+        for (int i = 0; i < s.length(); i++){
+            count[s.charAt(i) - 97] ++;
+        }
+        
+        for (int i = 0; i < 26; i++){
+            if (count[i] == 1) answer += (char)(i + 97);
+        }
+        return answer;
+    }
+}
