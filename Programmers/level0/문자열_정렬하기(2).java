8a1a461bbf6da393fd9a59ea8ffb7cc4c5e2753e
Im Seung-yeon <122198365+Imseungyeon@users.noreply.github.com>
2023-02-27 13:50:56 +0900

    Create 문자열_정렬하기(2).java

---
+++ Programmers/level0/문자열_정렬하기(2).java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String my_string) {
+        my_string = my_string.toLowerCase();
+        char[] c = my_string.toCharArray();
+        Arrays.sort(c);
+        return new String(c);
+    }
+}
