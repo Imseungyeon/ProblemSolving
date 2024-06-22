c1885e3f3f4bf54f6e5adddead3f2282d15d6d6b
Im Seung-yeon <122198365+Imseungyeon@users.noreply.github.com>
2024-06-22 23:54:30 +0900

    Update 더_맵게.py

--- Programmers/level2/더_맵게.py
+++ Programmers/level2/더_맵게.py
@@ -1,4 +1,4 @@
-# heapq 최소 힙 자동 정렬 이용
+# heapq 최소 힙(Min Heap) 자동 정렬 이용
 import heapq
 
 def solution(scoville, K):
@@ -10,6 +10,23 @@ def solution(scoville, K):
         count += 1
         sco = heapq.heappop(scoville) + heapq.heappop(scoville) * 2
         heapq.heappush(scoville, sco)
+
+    return count
+
+# 매번 전체 정렬을 필요로 하는 deque 활용시 효율성 떨어짐
+from collections import deque
+
+def solution(scoville, K):
+    q = deque(sorted(scoville))
+    count = 0
+    while q and q[0] < K:
+        if len(q) < 2:
+            return -1
+        count += 1
+        
+        sco = q.popleft() + q.popleft() * 2
+        q.append(sco)
+        q = deque(sorted(q)) 
 
     return count
   
